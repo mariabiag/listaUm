699b788d7588adbd8c7baad1df34cde51fa02bbc
Maria Beatriz <168425591+mariabiag@users.noreply.github.com>
2024-05-15 12:04:06 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,14 @@
+public class Calculadora {
+	private int somar(int a, int b) {
+		return a + b;
+	}
+	private int subtrair(int a, int b) {
+		return a - b;
+	}
+	private int multiplicar(int a, int b) {
+		return a * b;
+	}
+	private int dividir(int a, int b) {
+		return a / b;
+	}
+}
