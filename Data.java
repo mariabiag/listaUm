6ab10839cdb333fa28f3322c3218acdcf50636c8
Maria Beatriz <168425591+mariabiag@users.noreply.github.com>
2024-05-15 12:01:18 -0300

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,43 @@
+public class Data {
+
+private int dia;
+private int mes;
+private int ano;
+
+public Data (int novoDia, int novoMes, int novoAno){
+this.dia = novoDia;
+this.mes = novoMes;
+this.ano = novoAno;
+}
+public int getDia(){
+return dia;
+}
+
+public int getMes(){
+return mes;
+}
+
+public int getAno(){
+return ano;
+}
+public void setDia(int novoDia){
+this.dia = novoDia;
+}
+
+public void setMes (int novoMes){
+this.mes = novoMes;
+}
+
+public void setAno (int novoAno){
+this.ano = novoAno;
+}
+
+public String Mostrar(String a){
+return a;
+}
+
+public String imprimirInformacoes (){
+return "["  + this.dia + "]/[" + this.mes + "]/[" + this.ano + "]";
+}
+
+}
