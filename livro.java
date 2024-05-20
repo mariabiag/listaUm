079371cffd0c9265d4c7b61de1f749e91216998b
Maria Beatriz <168425591+mariabiag@users.noreply.github.com>
2024-05-20 08:15:42 -0300

    Create livro.java

---
+++ livro.java
@@ -0,0 +1,50 @@
+package testeAula;
+
+public class Livro {
+
+	private String titulo;
+	private String autor;
+	private String editora;
+	private int anoPublicacao;
+	
+	public Livro (String novoTitulo, String novoAutor, String novaEditora, int novoAnoPublicacao){
+		this.titulo = novoTitulo;
+		this.autor = novoAutor;
+		this.editora = novaEditora;
+		this.anoPublicacao = novoAnoPublicacao;
+
+		}
+	
+	public String getTitulo(){
+		return titulo;
+		}
+	
+	public String getAutor(){
+		return autor;
+		}
+	
+	public String getEditora(){
+		return editora;
+		}
+	
+	public int getAnoPublicacao(){
+		return anoPublicacao;
+		}
+	
+	public void setTitulo(String novoTitulo){
+		this.titulo = novoTitulo;
+		}
+
+		public void setAutor (String novoAutor){
+		this.autor = novoAutor;
+		}
+
+		public void setEditora (String novaEditora){
+		this.editora = novaEditora;
+		}
+		
+		public void setAnoPublicacao (String novoAnoPublicacao){
+			this.anoPublicacao = novoAnoPublicacao;
+			}
+
+}
