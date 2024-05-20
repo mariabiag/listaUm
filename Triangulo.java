92618ca09d787b55d0e58187a9770e43992f6688
Maria Beatriz <168425591+mariabiag@users.noreply.github.com>
2024-05-20 11:24:14 -0300

    Create Triangulo.java

---
+++ Triangulo.java
@@ -0,0 +1,43 @@
+public class Triangulo {
+
+	private double ladoA;
+	private double ladoB;
+	private double ladoC;
+	
+	public Triangulo (double novoLadoA, double novoLadoB, double novoLadoC) {
+		this.ladoA = novoLadoA;
+		this.ladoB = novoLadoB;
+		this.ladoC = novoLadoC;
+		}
+	
+	public double getLadoA() {
+		return ladoA;
+	}
+
+	public double getLadoB() {
+		return ladoB;
+	}
+	
+	public double getALadoC() {
+		return ladoC;
+	}
+	
+	public void setLadoA(double novoLadoA) {
+		this.ladoA = novoLadoA;
+	}
+	
+	public void setLadoB(double novoLadoB) {
+		this.ladoB = novoLadoB;
+}
+	public void setLadoC(double novoLadoC) {
+		this.ladoC = novoLadoC;
+	}
+	
+	public double calcularArea(double base, double altura) {
+	return (base * altura)/2;
+	}
+				
+ public double cacularPerimetro(double ladoA, double ladoB, double ladoC){
+	 return this.ladoA + this.ladoB + this.ladoC;
+ }
+}
